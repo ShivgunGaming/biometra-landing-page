7339799d804be0313e1239a9dc12d6e2d1103a25
AleksRG <AleksRDev@gmail.com>
2023-06-27 15:41:39 +0300

    Metadata update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -8,8 +7,6 @@ export const metadata = {
   title: "MP Locksmith Pro - Денонощен ключар и автоключар в София",
   description:
     "Предлагаме денонощни ключарски услуги в София. Свържете се с нас за бързо и надеждно отключване, смяна на ключове и други ключарски услуги.",
-  image:
-    "https://firebasestorage.googleapis.com/v0/b/hackathon-baa0c.appspot.com/o/MP.png?alt=media&token=51404a2a-7f88-4882-ab4b-3505c253cf40",
 };
 
 export default function RootLayout({
@@ -19,22 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="bg">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        <meta property="og:image:alt" content={metadata.title} />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="bg_BG" />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://mp-aleksrg.vercel.app/" />
-        <meta property="twitter:title" content={metadata.title} />
-        <meta property="twitter:description" content={metadata.description} />
-        <meta property="twitter:image" content={metadata.image} />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
