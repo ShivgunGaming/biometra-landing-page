e50cb428fec64184f8c85e3bb7d734d4407fe8fb
AleksRG <AleksRDev@gmail.com>
2023-06-28 13:31:09 +0300

    InfoBox add

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,25 +1,17 @@
 "use client";
-import {
-  ArrowUpRightIcon,
-  ArrowRightCircleIcon,
-} from "@heroicons/react/24/solid";
+import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
+import InfoBox from "./InfoBox";
 function Banner() {
   const handleClick = () => {
     window.open(
       "https://www.google.com/maps/place/%D1%83%D0%BB.+%E2%80%9E%D0%A0%D0%B0%D0%BB%D0%B5%D0%B2%D0%B8%D1%86%D0%B0%E2%80%9C+84,+1618+%D0%B6.%D0%BA.+%D0%9C%D0%B0%D0%BD%D0%B0%D1%81%D1%82%D0%B8%D1%80%D1%81%D0%BA%D0%B8+%D0%BB%D0%B8%D0%B2%D0%B0%D0%B4%D0%B8+-+%D0%91,+%D0%A1%D0%BE%D1%84%D0%B8%D1%8F/@42.6612846,23.2830126,19.25z/data=!4m5!3m4!1s0x40aa8495676145eb:0x65b8bba93efeefd0!8m2!3d42.6611736!4d23.2829039"
     );
   };
+  /* overflow-clip  */
   return (
     <div className="w-full items-center justify-center flex flex-col mt-10">
-      <div className="w-full max-w-[1350px] flex items-center justify-center px-8">
-        <div className="h-16 w-80 flex justify-center items-center border-none sm:border-dashed border-l border-r border-stone-800 -mb-16">
-          <div className="w-60 tracking-tight font-light border rounded-full border-white/10 text-zinc-400/90 flex space-x-2 items-center py-1.5 justify-center bg-stone-900/30 text-sm">
-            <ArrowRightCircleIcon className="w-5 h-5 text-sky-500" />
-
-            <span>24/7 София и региона</span>
-          </div>
-        </div>
-      </div>
+      <InfoBox />
+      <div className="w-full max-w-[1350px] flex items-center justify-center px-8"></div>
       <div className="w-full flex justify-center px-8">
         <div className="flex h-16 w-full max-w-7xl border-dashed border-l border-r border-stone-800 justify-between">
           <div className="h-16 w-16 border-r-pink-600 border rounded-full border-dashed border-stone-800 -ml-8 mt-8 rotate-45"></div>
