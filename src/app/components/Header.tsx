2d424a581a6e14a8eb44824df10ad090941048f8
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-06-30 11:16:29 +1000

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,21 +1,12 @@
 import React from "react";
-import { StaticImage } from "gatsby-plugin-image";
 
 function Header() {
-  const logoImage = (
-    <StaticImage
-      src="./biometra.png"
-      alt="Logo"
-      className="h-8"
-    />
-  );
 
   return (
     <div className="px-5 py-2 flex cursor-pointer border-b border-white/5 w-full justify-center bg-black/70 backdrop-blur-md items-center">
       <div className="max-w-[1350px] w-full text-sm font-light tracking-tight justify-between">
         <h1 className={`mb-3 text-2xl font-semibold whitespace-nowrap`}>
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none font-extrabold tracking-tight text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 via-zinc-100/80 to-white">
-            {logoImage}
             Biometra
           </span>
         </h1>
