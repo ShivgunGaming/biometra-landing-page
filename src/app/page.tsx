0b173e68ae2a8c3afdb75c7d74636f3eba2acb90
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-06-30 10:36:26 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,75 +12,28 @@ export default function Home() {
       <Banner />
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:from-white  before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic  after:via-blue-200 after:blur-2xl after:content-[''] before:bg-gradient-to-br before:dark:from-transparent before:to-sky-700 before:dark:opacity-10 after:from-sky-900 after:dark:via-[#0141ff] after:opacity-40 before:lg:h-[360px]">
         <div className="flex flex-wrap gap-6 w-full h-full justify-center my-10 px-2">
-          <Box name="Гаранция" description="12 месеца за всички дейности." />
-          <Box name="Пристигане" description="София - аварийно до 30 мин." />
+          <Box name="About Us" description="Welcome to Biometra, your trusted Biometric Scanning Web3 Wallet." />
         </div>
       </div>
       <div className="flex flex-wrap gap-8 w-full h-full justify-center my-10 ">
         <Service
-          title="Жилищен Ключар"
+          title="Secure Biometric Authentication"
           items={[
-            "Дубликат на всички видове ключове.",
-            "Изработка на ключ по брава, патрон.",
-            "Ремонт на патрони, брави и дръжки.",
-            "Преработване на заключващи системи.",
-            "Прекодиране на касови брави.",
-            "Дубликат на чипове за контрол на достъп.",
-            "Одит на сигурността за Вашето жилище.",
+            "Biometra offers advanced biometric scanning technology to ensure secure and convenient authentication for your Web3 transactions. Say goodbye to passwords and enjoy seamless access with just a touch or a glance.",
           ]}
         />
         <Service
-          title="Автомобилен Ключар"
+          title="Effortless Transaction Management"
           items={[
-            "Отключване на автомобил.",
-            "Изработка на автоключове.",
-            "Изтриване на ключ от паметта на имобилайзер система.",
-            "Обучение на имобилайзер система да познава нов чип.",
-            "Прекодиране на ключалка на врата, багажник, резервоар и контакт.",
-            "Дубликат и ремонт на дистанционни.",
+            "With Biometra, managing your Web3 transactions becomes effortless. Our intuitive interface allows you to easily send, receive, and monitor your digital assets, providing a smooth and user-friendly experience.",
           ]}
         />{" "}
         <Service
-          title="Авариен Ключар"
+          title="Unparalleled Security"
           items={[
-            "Заключен e дома Ви?",
-            "Вашата ключалка счупена ли е?",
-            "Изгубен, откраднат или повреден ключ?",
-            "Нуждаете се от авариен ключар? Нашите мобилни ключари извършват дискретни, аварийни отвори и ремонти в цяла София и околностите.",
-            "Свържете се с нас за безплатна консултация.",
+            "Your digital assets deserve the highest level of security. Biometra employs cutting-edge encryption and decentralized storage to safeguard your funds and personal information, giving you peace of mind and protection against cyber threats.",
           ]}
         />{" "}
-        <Service
-          title="Интелигентни Брави"
-          items={[
-            "Продажба, монтаж и сервиз на умна ключалка.",
-            "Отключване по най-безопасния начин.",
-            "Смяна на кода за брава.",
-            "Дубликат на дистанционно, включително и Rolling code.",
-            "Wi-fi сензор за врати и прозорци.",
-            "Инсталиране и обучение на приложението за управление.",
-          ]}
-        />{" "}
-        <Service
-          title="Търговски Ключар"
-          items={[
-            "Одит на търговски и бизнeс обектиа.",
-            "Продажба и монтаж на сейфове и каси.",
-            "Подмяна и прекодиране на заключващ механизам на каси и сейфове.",
-            "Продажба на патрон и брава от high-security клас.",
-            "Профилактика и отключване на заключващи системи от висок клас.",
-          ]}
-        />{" "}
-        <Service
-          title="Врати и Прозорци От PVC"
-          items={[
-            "Монтаж на заключващи системи и точкови райбери.",
-            "Монтаж на обикновен, с блокировка и smart райбер.",
-            "Регулиране и подмяна на механизъм-обков на врати и прозорци.",
-            "Интегриране на допълнителна заключваща система.",
-            "Монтаж на невидима ключалка с дистанционно.",
-          ]}
-        />
       </div>
       <Footer />
     </main>
