79b5b842c3384278842a3983241d6dcd331190f0
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-07-06 19:10:45 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Service from "./components/Service";
 import Stars from "./components/Stars";
+/* import Logo from "./components/Logo"; */
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Header />
-      <Stars />
       <Banner />
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:from-white  before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic  after:via-blue-200 after:blur-2xl after:content-[''] before:bg-gradient-to-br before:dark:from-transparent before:to-sky-700 before:dark:opacity-10 after:from-sky-900 after:dark:via-[#0141ff] after:opacity-40 before:lg:h-[360px]">
         <div className="flex flex-wrap gap-6 w-full h-full justify-center my-10 px-2">
