0683008f0fa5c22edd9dd18c690d129bef39b2b4
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-08-23 13:23:48 +1000

    Updated InfoBox Added Chrome Web Store Link

--- src/app/components/InfoBox.tsx
+++ src/app/components/InfoBox.tsx
@@ -2,6 +2,8 @@ import { ArrowRightCircleIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
 function InfoBox() {
+  const link = "https://chrome.google.com/webstore/detail/biometra/geeojnpimoehffcokhkikhbobigpffck";
+
   return (
     <div className="h-16 w-80 flex justify-center items-center border-none sm:border-dashed border-l border-r border-stone-800 -mb-16">
       <div className="relative h-9 group my-10 w-60 rounded-full justify-center flex items-center overflow-clip">
@@ -11,10 +13,15 @@ function InfoBox() {
             animationDuration: "4s",
           }}
         ></div>
-        <div className="w-60 relative tracking-tight font-light border rounded-full border-white/10 text-zinc-400/90 flex space-x-2 items-center py-1.5 justify-center bg-zinc-950 text-sm">
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-60 relative tracking-tight font-light border rounded-full border-white/10 text-zinc-400/90 flex space-x-2 items-center py-1.5 justify-center bg-zinc-950 text-sm"
+        >
           <ArrowRightCircleIcon className="w-5 h-5 text-sky-500" />
-          <span>Biometric Authentication</span>
-        </div>
+          <span>Check us out, LFG 🚀</span>
+        </a>
       </div>
     </div>
   );
